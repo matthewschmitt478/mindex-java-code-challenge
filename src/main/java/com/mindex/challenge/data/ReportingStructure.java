61acc13609a113ce467d95f308b38bac45b4f064
Mschm <mjs6409@g.rit.edu>
2020-10-25 15:44:59 -0400

    Commiting ReportingStructure to the Github

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -1,4 +1,25 @@
 package com.mindex.challenge.data;
 
 public class ReportingStructure {
+    private Employee employee;
+    private int numberOfReports;
+
+    public ReportingStructure() {
+    }
+
+    Employee getEmployee() {
+        return this.employee;
+    }
+
+    void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    int getNumberOfReports() {
+        return this.numberOfReports;
+    }
+
+    void setNumberOfReports(int numberOfReports) {
+        this.numberOfReports = numberOfReports;
+    }
 }
